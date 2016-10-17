db833cb0f4b2fd413a34b748187da566e43fbc3e
Diego Haz <hazdiego@gmail.com>
2016-10-17 21:36:06 -0200

    Update code to transpile on the fly

--- generators/app/templates/src/index.js
+++ generators/app/templates/src/index.js
@@ -1,7 +1,3 @@
-var env = process.env.NODE_ENV || 'development'
-
-if (env === 'development' || env === 'test') {
-  require('babel-core/register')
-}
+require('babel-core/register')
 
 exports = module.exports = require('./app')
